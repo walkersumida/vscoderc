adc6259f8e4aa5cdb8158073d0f355cc6a884ec3
WalkerSumida <walkersumida@gmail.com>
2020-09-24 11:36:55 +0900

    chore: add shell file for commit

---
+++ copy_files_referenced_by_vscode_app.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+cp -f ~/Library/Application\ Support/Code/User/keybindings.json ./keybindings.json
+cp -f ~/Library/Application\ Support/Code/User/settings.json ./settings.json
+
+echo "Done"
+
